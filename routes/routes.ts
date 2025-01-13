b4ea58c0a32368ba4cf312c939f345c5d4e8df17
adhimrahman <adhimrahaman@gmail.com>
2025-01-13 21:18:57 +0800

    feat: add router

---
+++ routes/routes.ts
@@ -0,0 +1,17 @@
+import express, { Router } from "express";
+import schedule from "../controllers/schedule";
+import member from "../controllers/member";
+import news from "../controllers/news";
+import birthday from "../controllers/birthday";
+
+const router: Router = express.Router();
+
+router.get("/schedule", schedule.getSchedule);
+router.get("/schedule/detail/:idschedule", schedule.getDetailSchedule);
+router.get("/schedule/theater", schedule.getTheaterSchedule);
+router.get("/member", member.getAllMember);
+router.get("/member/detail/:idmember", member.getDetailMember);
+router.get("/news", news.getAllNews);
+router.get("/birthday", birthday.listBirthDay);
+
+export default router;

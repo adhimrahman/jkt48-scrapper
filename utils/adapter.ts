af93fcb9007048f32f7bf9eae6141c9ad5821b63
adhimrahman <adhimrahaman@gmail.com>
2025-01-14 12:18:40 +0800

    feat : fix error handling

--- utils/adapter.ts
+++ utils/adapter.ts
@@ -1,12 +1,14 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export const serverlessToExpress = (
-    handler: (req: VercelRequest, res: VercelResponse) => Promise<void>
-) => {
-    return async (req: Request, res: Response) => {
-        const vercelReq = req as unknown as VercelRequest;
-        const vercelRes = res as unknown as VercelResponse;
-        await handler(vercelReq, vercelRes);
+export const serverlessToExpress =
+    (handler: (req: VercelRequest, res: VercelResponse) => Promise<void>) =>
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const vercelReq: VercelRequest = req as unknown as VercelRequest;
+            const vercelRes: VercelResponse = res as unknown as VercelResponse;
+            await handler(vercelReq, vercelRes);
+        } catch (error) {
+            next(error);
+        }
     };
-};
